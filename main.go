e42119606309582973b1825748791b78a66213d7
Rodrigo Dionizio <rodrigosantiagodionizio@hotmail.com>
2021-07-18 16:34:45 -0300

    Convertendo a slice de struct Livro em json com a biblioteca encoding/json

--- main.go
+++ main.go
@@ -1,13 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type Livro struct {
+	Id     int
+	Titulo string
+	Autor  string
+}
+
+var Livros []Livro = []Livro{
+	{
+		Id:     1,
+		Titulo: "Capitães de Areia",
+		Autor:  "Jorge Amado",
+	},
+	{
+		Id:     2,
+		Titulo: "Dona Flor e seus dois Maridos",
+		Autor:  "Jorge Amado",
+	},
+	{
+		Id:     3,
+		Titulo: "Porto dos Milagres",
+		Autor:  "Jorge Amado",
+	},
+}
+
 func configurarRotas() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Sejam todos Bem-vindos")
+	})
+	http.HandleFunc("/livros", func(w http.ResponseWriter, r *http.Request) {
+		encoder := json.NewEncoder(w)
+		encoder.Encode(Livros)
 	})
 }
 
